6c706e4f833aeee6143021fac94284645ffd0612
shekhkhwaja <khwajajulaha122@gmail.com>
2025-04-11 10:34:11 +0530

    test commit

--- index.java
+++ index.java
@@ -1,6 +1,6 @@
 class hello {
     public static void main(String[] args) {
         System.out.println("hello this git ");
-        System.out.println("raja sonker");
+        System.out.println("raja sonker ");
     }
 }
