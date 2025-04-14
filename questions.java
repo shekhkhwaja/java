269808ffbe89d5945f08ec14d4d9e7ff269a1965
shekhkhwaja <khwajajulaha122@gmail.com>
2025-04-14 15:11:33 +0530

    today

---
+++ questions.java
@@ -0,0 +1,12 @@
+public class questions {
+    void khwaja()
+    
+
+
+
+
+
+
+
+    
+}
---
+++ student.java
@@ -0,0 +1,11 @@
+public class student {
+    void sum()
+{
+
+}
+    public static void main(String[] args)
+    {
+        student obj=new student();
+        System.out.print(obj);
+    }
+}
