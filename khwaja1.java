18d8fd11742e24291f961f1f5625e4b4c17ac19b
shekhkhwaja <khwajajulaha122@gmail.com>
2025-04-28 13:55:53 +0530

    hello

---
+++ khwaja1.java
@@ -0,0 +1,10 @@
+class khwaja1 {
+     static void sum(int a) {
+
+        System.out.println(a);
+     }
+    public static void main(String[] args) {
+        sum(5);
+    }
+    
+}
---
+++ sonu1.java
@@ -0,0 +1,12 @@
+ class sonu1 {
+     void sum(int a )
+    {
+  System.out.println(a);
+    }
+public void main(String[] args) {
+    sonu1 obj=new sonu1();
+    obj.sum(5);
+
+}
+    
+}
